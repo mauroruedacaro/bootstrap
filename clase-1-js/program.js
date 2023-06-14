66af4828799a78538f833d28b8c539bcd2b85edd
mauruecar64 <mruedaca8006@universidadean.edu.co>
2023-06-14 11:10:37 -0500

    arreglos

---
+++ clase-1-js/program.js
@@ -0,0 +1,35 @@
+console.log ('el valor de la operacion es ',2+3 );
+console.log ('el valor de la operacion es: ' + (4*8) + " Resultado")
+// salidas
+// concateno string con + "string1" + "strnog2"
+document.write  ('soy un texto que me adiciono al html'); // colocarlo en una etuoqueta p
+// alert   ('sot una ventana');
+// Las entradas son información a recibir
+const age = prompt ('Ingresar por favo tu año nacimient');
+const birthday = 2023 - age;
+console.log (' tienes ...' + birthday + ' años');
+
+// Pasar texto  numero
+const age1 = parseInt ('algo');
+console.log (age1);
+// boolean: Best practice is number valor with one "has" o "have" o to be
+// Exampe 
+const isUserRegister = false;
+const hasInformation = true;
+// null
+latePaperRoll = null;
+// undefined
+let someThing;
+console.log (' Some Thing ', someThing);
+//objetos
+const carMy = {
+    doors : 4,
+    brand : 'bmw',
+    capacity : 5,
+};
+// Operaciones matematicas
+let result = 5+5;
+let resultMin = 8-5;
+let resultMult = 8*5;
+let resultDiv = 8/2;
+let resultSq = 8**8;
